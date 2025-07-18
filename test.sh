bc17633b5df051aa376835460047e64009338c7c
Brandon Link <blink4.535743.jhu@gmail.com>
2025-07-17 18:20:04 -0600

    Update test.sh for Google Colab directory

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python run.py \
-    --ckpt /home/vince/HMDB_Vid_Classification/models/epoch4_model_wts.pt \
+    --ckpt /content/Action_Recognition/models/best_model_wts.pt \
     --model_type lrcn \
     --n_classes 51 \
     --model_type lrcn \
